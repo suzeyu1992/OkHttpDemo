237f71312e9f501c3965d041ec8935128244cf68
suzeyu <suzeyu_1992@163.com>
2017-01-25 17:06:04 +0800

    post请求方式

--- app/src/main/java/com/szysky/img/okhttpdemo/MainActivity.java
+++ app/src/main/java/com/szysky/img/okhttpdemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.szysky.img.okhttpdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.JsonReader;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
@@ -10,8 +11,10 @@ import java.io.IOException;
 
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
@@ -51,5 +54,51 @@ public class MainActivity extends AppCompatActivity {
                 response.close();
             }
         });
+    }
+
+
+    /**
+     * 发送post请求
+     */
+    public void send_post(View tv_post){
+         String url = "http://www.roundsapp.com/post" ;
+
+        // post请求的 请求体内容
+        String json = "{'winCondition':'HIGH_SCORE',"
+                + "'name':'Bowling',"
+                + "'round':4,"
+                + "'lastSaved':1367702411696,"
+                + "'dateStarted':1367702378785,"
+                + "'players':["
+                + "{'name':'" + "Jesse" + "','history':[10,8,6,7,8],'color':-13388315,'total':39},"
+                + "{'name':'" + "Jake" + "','history':[6,10,5,10,10],'color':-48060,'total':41}"
+                + "]}";
+
+         // 设置请求体内容
+        // 可以指定的编码字符集有字节数组, 文件, 字符串等类型
+        RequestBody requestBody = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), json);
+
+        OkHttpClient okHttpClient = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .post(requestBody)
+                .build();
+
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.i(TAG, "post请求失败:  "+e);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.i(TAG, "post请求成功: "+ response.toString());
+                Log.i(TAG, "post请求成功: "+ response.body().string());
+                response.close();
+
+            }
+        });
+
     }
 }
